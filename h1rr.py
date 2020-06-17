495998066801b92728f414c4059783c7e99c2abf
Ahmet Husrev Ceker <ahusrevceker@gmail.com>
2020-06-17 04:07:09 +0300

    Update h1rr to v0.2
    +You can create a txt file that includes the wordlist. (It is created in home directorty for this version)
    +Now, client of h1rr is completely English.
    -setup.py still does not work.

--- h1rr.py
+++ h1rr.py
@@ -1,4 +1,3 @@
-
 from itertools import permutations 
  
 print("h1rr v0.1")
@@ -6,19 +5,32 @@ print("h1rr v0.1")
 i = ''
 veriler = []
 
-print("cik komutu ile eklenti vermeyi bitirebilirsiniz")
+print("You can end giving keys with 'cik' command")
 while i != 'cik':
-    i = input("Eklenti giriniz : ")
+    i = input("Give a key : ")
     veriler.append(i) 
     print(veriler) 
 veriler.pop()
 
-limit = int(input("Bir arada kullanilacak max eklenti sayisini seciniz : "))
+limit = input("Choose the value of delimeter that will determine how many keys will be used\n(If you do not give a number, it will use all the keys)\n: ")
+if limit == '':
+    limit = int(len(veriler))
+limit = int(limit)
 
+filename = input("Enter your file name if you want to create a txt file that includes the wordlist on your desktop\n(If you do not enter a name, it will not create a file that includes the wordlist)\n: ")
+ 
 for n in range(1,limit+1):
     perm = permutations(veriler, n) 
   
  
     for i in list(perm): 
-        print("".join(list(i)))
+        thekey = "".join(list(i))
+        print(thekey)
+        
+        if filename == '':
+            continue
+        else:
+            file = open(f"{filename}.txt", "a") # burada masaustune olusturmam gerekiyor!!
+            file.write(f"{thekey}\n") 
+file.close() 
     
